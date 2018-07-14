f42476ee0a5b7be9ce382b0e0f4cd3b919e9782c
Max Li <maxlivinci@gmail.com>
2018-07-14 21:58:43 +0800

    comment out some routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const respond = require('koa-respond');
 
-const ImageCache = require('./class/ImageCache');
-
 const { IMAGE_WIDTH, IMAGE_HEIGHT } = require('./constants');
+
+const ImageCache = require('./class/ImageCache');
 
 const imageCache = new ImageCache({
   width: IMAGE_WIDTH,
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 app.use(respond());
 
-let router = new Router();
+const router = new Router();
 
 router.get('/tumbnail/:postid/:filename', async ctx => {
   const postId = ctx.params.postid;
@@ -32,23 +32,23 @@ router.get('/status', async ctx => {
   ctx.ok(imageCache.getStatus());
 });
 
-router.get('/test1', async ctx => {
-  const reg_post1 = require('./data/reg_post1');
-  await imageCache.start(reg_post1);
-  console.log('reg_image', imageCache.reg_image);
-  ctx.ok('OK');
-});
-
-router.get('/test2', async ctx => {
-  const reg_post2 = require('./data/reg_post2');
-  await imageCache.start(reg_post2);
-  console.log('reg_image', imageCache.reg_image);
-  ctx.ok('OK');
-});
-
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, err => {
   if (err) throw err;
   console.log(`> ready on http://localhost:${port}`);
 });
+
+// router.get('/test1', async ctx => {
+//   const reg_post1 = require('./data/reg_post1');
+//   await imageCache.start(reg_post1);
+//   console.log('reg_image', imageCache.reg_image);
+//   ctx.ok('OK');
+// });
+
+// router.get('/test2', async ctx => {
+//   const reg_post2 = require('./data/reg_post2');
+//   await imageCache.start(reg_post2);
+//   console.log('reg_image', imageCache.reg_image);
+//   ctx.ok('OK');
+// });
